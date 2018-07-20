b39a471561808ec4f0fe2233827d978ed4a9ecc0
TeamKeepers <team@keepizi.com>
2018-07-20 09:35:01 +0200

    Add comments to the existent class

--- europe/app/AbstractApplication.php
+++ europe/app/AbstractApplication.php
@@ -8,12 +8,43 @@ use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 use Symfony\Component\HttpFoundation\Request;
 
+/**
+ * This class is the main application skeleton. It's used to define the base workflow of the request.
+ * You'll have to overwrite the internal "run" method to implement your own logic.
+ * 
+ * @author matthieu vallance <matthieu.vallance@vesperiagroup.com>
+ */
 abstract class AbstractApplication
 {
+    /**
+     * Logger
+     * 
+     * The application logger, to store the workflow informations
+     * 
+     * @var Logger
+     */
     private $logger;
     
+    /**
+     * Request
+     * 
+     * The current server request, comming form the client
+     * 
+     * @var Request
+     */
     private $request;
     
+    /**
+     * Construct
+     * 
+     * The default Application constructore, used to define the logger file storage
+     * 
+     * @param Request $request  The current server request
+     * @param string  $logPath  The logger file
+     * @param int     $logLevel The level of log to store (DEBUG, WARNING, ERROR, etc...)
+     * 
+     * @return void
+     */
     public function __construct(Request $request, string $logPath, int $logLevel = Logger::DEBUG)
     {
         $this->logger = new Logger('App');
@@ -22,6 +53,14 @@ abstract class AbstractApplication
         $this->request = $request;
     }
     
+    /**
+     * Execute
+     * 
+     * Start the execution of the request workflow. Register an exception displayer to help debuging
+     * 
+     * @throws \Exception 
+     * @return Response
+     */
     public function execute()
     {
         $whoops = new Run();
@@ -42,19 +81,50 @@ abstract class AbstractApplication
         }
     }
     
+    /**
+     * Get request
+     * 
+     * Return the current server request
+     * 
+     * @return Request
+     */
     protected function getRequest() : Request
     {
         return $this->request;
     }
     
+    /**
+     * Logger
+     * 
+     * Return the current application logger
+     * 
+     * @return Logger
+     */
     protected function getLogger() : Logger
     {
         return $this->logger;
     }
     
+    /**
+     * Configure the response
+     * 
+     * If you need to set the headers or modify the response body before sending it to the client,
+     * you can overwrite this method.
+     * 
+     * @param Response $response The response to be sent to the client
+     * 
+     * @return void
+     */
     protected function configureResponse(Response $response)
     {}
     
+    /**
+     * Run
+     * 
+     * Execute the request process
+     * 
+     * @return string|NULL
+     */
     protected abstract function run() : ?string;
 }
 
--- europe/src/Application.php
+++ europe/src/Application.php
@@ -1,9 +1,21 @@
 <?php
 use App\AbstractApplication;
 
+/**
+ * This class is the main application. You will overwrite the "run" method to implement your code
+ *
+ * @author matthieu vallance <matthieu.vallance@vesperiagroup.com>
+ */
 class Application extends AbstractApplication
 {
-    protected function run() : string
+    /**
+     * Run
+     *
+     * Execute the request process
+     *
+     * @return string|NULL
+     */
+    protected function run() : ?string
     {
         return 'Please, insert your code inside the src/Application::run method';
     }
