ea05eeca93733e632898dc76bb7b48ac904b5e5d
TeamKeepers <team@keepizi.com>
2018-07-20 08:33:18 +0200

    Improve request and response capability

--- europe/app/AbstractApplication.php
+++ europe/app/AbstractApplication.php
@@ -6,16 +6,24 @@ use Whoops\Handler\PrettyPageHandler;
 use Symfony\Component\HttpFoundation\Response;
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
+use Symfony\Component\HttpFoundation\Request;
 
 abstract class AbstractApplication
 {
-    protected $logger;
+    private $logger;
+    
+    private $request;
+    
+    public function __construct(Request $request, string $logPath, int $logLevel = Logger::DEBUG)
+    {
+        $this->logger = new Logger('App');
+        $this->logger->pushHandler(new StreamHandler($logPath, $logLevel));
+        
+        $this->request = $request;
+    }
     
     public function execute()
     {
-        $this->logger = new Logger('App');
-        $this->logger->pushHandler(new StreamHandler(__DIR__.DIRECTORY_SEPARATOR.'logs'.DIRECTORY_SEPARATOR.'dev.log', Logger::DEBUG));
-        
         $whoops = new Run();
         $whoops->pushHandler(new PrettyPageHandler());
         $whoops->register();
@@ -24,12 +32,28 @@ abstract class AbstractApplication
             $this->logger->debug('Launch the application run');
             $result = $this->run();
             $this->logger->debug('No errors', ['result length' => strlen($result)]);
-            return new Response($result);
+            $response = new Response($result);
+            $this->configureResponse($response);
+            
+            return $response;
         } catch (\Exception $exception) {
             $this->logger->debug('Error reported', ['message' => $exception->getMessage(), 'trace' => $exception->getTrace()]);
             throw $exception;
         }
     }
+    
+    protected function getRequest() : Request
+    {
+        return $this->request;
+    }
+    
+    protected function getLogger() : Logger
+    {
+        return $this->logger;
+    }
+    
+    protected function configureResponse(Response $response)
+    {}
     
     protected abstract function run() : ?string;
 }
--- europe/public/index.php
+++ europe/public/index.php
@@ -1,7 +1,12 @@
 <?php 
 
+use Symfony\Component\HttpFoundation\Request;
+
 require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
-$application = new Application();
+$application = new Application(
+    Request::createFromGlobals(),
+    __DIR__.DIRECTORY_SEPARATOR.'logs'.DIRECTORY_SEPARATOR.'dev.log'
+);
 
 $application->execute()->send();
